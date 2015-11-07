8dfa13962fb4dac2acbbb1980de7008dd5150d08
Christian Neal <c.neal91@gmail.com>
2015-11-07 16:07:32 -0600

    Finish Chap 3

---
+++ chap3/string.rb
@@ -0,0 +1,22 @@
+puts 'Hello, World'
+puts ''
+puts 'Good-bye'
+puts ''
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4
+puts ''
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts ''
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+# puts ''
+# puts '12' + 12
+# puts '2' * '5'
+puts 'You\'re swell!'
+puts 'up\\down'
+puts 'up\down'
