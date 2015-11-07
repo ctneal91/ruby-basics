ff3cfa4602c84536af068a8b7cca0cae170ddbcf
Christian Neal <c.neal91@gmail.com>
2015-11-07 15:50:07 -0600

    Chapter 2 complete

---
+++ chap2/calc.rb
@@ -0,0 +1,22 @@
+puts "2.1"
+puts 1+2
+puts ""
+puts "2.4"
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts "vs"
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+puts "2.5"
+puts "Hours in a year:"
+puts 24 * 365
+puts "Minutes in a decade:"
+puts 60 * 24 * 365 * 10 + 60 * 24 * 2
+puts "My age in seconds:"
+puts 60 * 60 * 24 * 365 * 23 + 60 * 60 * 24 * 30 * 3
+puts "Author's age in years:"
+puts 1160000000 / (60 * 60 * 24 * 365)
