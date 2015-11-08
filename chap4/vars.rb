cb8e01d2d04af0402b87b426648bfd18363e29d8
Christian Neal <c.neal91@gmail.com>
2015-11-08 16:30:27 -0600

    Add chap4

---
+++ chap4/vars.rb
@@ -0,0 +1,19 @@
+name = 'Christian Tanner Neal'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name.'
+puts ''
+composer = 'Mozart'
+puts composer + ' was the "da bomb" in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+puts ''
+var1 = 8
+var2 = var1
+puts var1
+puts var 2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
