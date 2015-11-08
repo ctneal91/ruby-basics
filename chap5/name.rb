e88bda9d3efce24fb54ab54565573d4e183796a0
Christian Neal <c.neal91@gmail.com>
2015-11-08 17:17:08 -0600

    chapter 5 complete

---
+++ chap5/name.rb
@@ -0,0 +1,17 @@
+# puts 'Hello there, and what\'s your name?'
+# name = gets.chomp
+# puts 'Your name is ' + name + '? What a lovely name.'
+# puts 'Please to meet you, ' + name + '. :)'
+#
+# puts 'What is your mother\'s first name?'
+# firstname = gets.chomp
+# puts 'Middle name?'
+# middlename = gets.chomp
+# puts 'Last name?'
+# lastname = gets.chomp
+# puts 'Well, I am sure that ' + firstname + ' ' + middlename + ' ' + lastname + ' is a wonderful woman.'
+
+puts 'What is your favorite number?'
+number = gets.chomp
+better_number = number.to_i + 1
+puts 'Isn\'t ' + better_number.to_s + ' a better number, though?'
