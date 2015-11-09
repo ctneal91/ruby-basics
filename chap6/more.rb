d1055a21cae1aa6fb127f3b64885a757249ca4c4
Christian Neal <c.neal91@gmail.com>
2015-11-09 09:48:53 -0600

    chap6 complete

---
+++ chap6/more.rb
@@ -0,0 +1,40 @@
+# var1 = 'stop'
+# var2 = 'deliver repaid desserts'
+# var3 = '....TCELES B HSUP A magic spell?'
+#
+# puts var1.reverse
+# puts var2.reverse
+# puts var3.reverse
+# puts var1
+# puts var2
+# puts var3
+#
+# puts 'What is your full name?'
+# name = gets.chomp
+# length = name.length - 2
+# puts 'Did you know there are ' + length.to_s + ' letters in your name, ' + name + '?'
+
+# letters = 'aAbBcCdDeE'
+# puts letters.upcase
+# puts letters.downcase
+# puts letters.swapcase
+# puts letters.capitalize
+# puts ' a'.capitalize
+# puts letters
+#
+# line_width = 50
+# puts ('A white plastic spork'.center(line_width))
+# puts('Met 3 silver spoons last night'.center(line_width))
+# puts('Stuck-up spoons are dumb.'.center(line_width))
+
+# puts "What do you want?"
+# request = gets.chomp
+# puts 'WHADDAYA MEAN \'' + request.upcase + '\'?!? YOU\'RE FIRED!!!'
+
+chap = 'Chapter'
+width = 60
+puts('Table of Contents'.center(width))
+puts ''
+puts((chap + ' 1:  Getting Started').ljust(width/2) + 'page 1'.rjust(width/2))
+puts((chap + ' 2:  Numbers').ljust(width/2) + 'page 9'.rjust(width/2))
+puts((chap + ' 3:  Letters').ljust(width/2) + 'page 13'.rjust(width/2))
